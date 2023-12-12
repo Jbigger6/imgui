184b428a54189becb387a84f4ed6df8b18966a0a
Joseph Bigger <joseph.bigger1@outlook.com>
2023-12-12 11:50:38 -0600

    Refactor Window class

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,7 @@ project "ImGui"
     files
     {
         "imconfig.h",
+        "imgui.h",
         "imgui.cpp",
         "imgui_draw.cpp",
         "imgui_internal.h",
@@ -15,7 +16,8 @@ project "ImGui"
         "imgui_widgets.cpp",
         "imstb_rectpack.h",
         "imstb_textedit.h",
-        "imstb_truetype.h"
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
     }
 
     filter  "system:windows"
